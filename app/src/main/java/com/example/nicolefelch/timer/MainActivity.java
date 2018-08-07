a47c84c55aed00a3e85c0753ae6a606a9061c34b
nf1213 <nicole@oviahealth.com>
2018-08-07 16:20:14 -0400

    Add counter action

--- app/src/main/java/com/example/nicolefelch/timer/MainActivity.java
+++ app/src/main/java/com/example/nicolefelch/timer/MainActivity.java
@@ -4,8 +4,11 @@ import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.SystemClock;
@@ -14,14 +17,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Chronometer chronometer;
     private Button startStop;
+    private TextView count;
 
     private static final String NOTIFICATION_CHANNEL = "com.example.nicolefelch.timerapp";
     private static final String KEY_CHRONOMETER_BASE = "key_base";
+    private static final String KEY_COUNT = "key_count";
     private static final String PREF_FILE = "prefs";
 
     @Override
@@ -40,17 +46,19 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+        count = findViewById(R.id.count);
+        count.setText(String.valueOf(getCount()));
     }
 
     private void stopTimer() {
-        getSharedPreferences(PREF_FILE, Context.MODE_PRIVATE).edit().putLong(KEY_CHRONOMETER_BASE, -1).apply();
+        setChronometerBase(-1);
         chronometer.stop();
         startStop.setText(R.string.start);
     }
 
     private void startTimer(long base) {
         chronometer.setBase(base);
-        getSharedPreferences(PREF_FILE, Context.MODE_PRIVATE).edit().putLong(KEY_CHRONOMETER_BASE, chronometer.getBase()).apply();
+        setChronometerBase(chronometer.getBase());
         chronometer.start();
         startStop.setText(R.string.stop);
     }
@@ -58,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStop() {
         super.onStop();
-        long base = getSharedPreferences(PREF_FILE, Context.MODE_PRIVATE).getLong(KEY_CHRONOMETER_BASE, -1);
+        long base = getChronometerBase();
         if (base > 0) startNotification(base);
     }
 
@@ -66,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         cancelNotification();
-        long base = getSharedPreferences(PREF_FILE, Context.MODE_PRIVATE).getLong(KEY_CHRONOMETER_BASE, -1);
+        long base = getChronometerBase();
         if (base > 0) startTimer(base);
     }
 
@@ -76,9 +84,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void startNotification(long chronometerBase) {
+        Intent heartIntent = new Intent(MainActivity.this, CountReceiver.class);
+        PendingIntent heartAction = PendingIntent.getBroadcast(MainActivity.this, 0, heartIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL)
                 .setContentTitle("Timer")
                 .setSmallIcon(R.mipmap.ic_launcher)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.cat))
+                .setStyle(new android.support.v4.media.app.NotificationCompat.MediaStyle()
+                        .setShowActionsInCompactView(0))
+                .addAction(R.drawable.ic_heart, "Love", heartAction)
                 .setUsesChronometer(true)
                 .setOngoing(true)
                 .setAutoCancel(true)
@@ -89,5 +104,27 @@ public class MainActivity extends AppCompatActivity {
         if (Build.VERSION.SDK_INT >=Build.VERSION_CODES.O) notificationManager.createNotificationChannel(new NotificationChannel(NOTIFICATION_CHANNEL, getString(R.string.notification_channel_name), NotificationManager.IMPORTANCE_HIGH));
         notification.when = System.currentTimeMillis() - (SystemClock.elapsedRealtime() - chronometerBase);
         notificationManager.notify(0, notification);
+    }
+
+    private int getCount() {
+        return getSharedPreferences(PREF_FILE, MODE_PRIVATE).getInt(KEY_COUNT, 0);
+    }
+
+    private void setChronometerBase(long i) {
+        getSharedPreferences(PREF_FILE, Context.MODE_PRIVATE).edit().putLong(KEY_CHRONOMETER_BASE, i).apply();
+    }
+
+    private long getChronometerBase() {
+        return getSharedPreferences(PREF_FILE, Context.MODE_PRIVATE).getLong(KEY_CHRONOMETER_BASE, -1);
+    }
+
+    public static class CountReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // increment count
+            SharedPreferences prefs = context.getSharedPreferences(PREF_FILE, Context.MODE_PRIVATE);
+            int count = prefs.getInt(KEY_COUNT, 0);
+            prefs.edit().putInt(KEY_COUNT, count + 1).apply();
+        }
     }
 }
